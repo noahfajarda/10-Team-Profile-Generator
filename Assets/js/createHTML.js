0e69259955e368220e24def8351f3a2a79071ce4
Noah Fajarda <noahfajarda@gmail.com>
2022-12-05 20:32:47 -0800

    added logic to specific names

--- Assets/js/createHTML.js
+++ Assets/js/createHTML.js
@@ -8,15 +8,19 @@ function arrayRemove(arr, value) {
     });
 }
 
+function arrayRemoveAlt(arr, val) {
+    arr.splice(arr.indexOf(val), 1);
+}
+
 // get indicies if its 'male' or 'female'
 function getIndicies(count) {
     var imageNumber = Math.floor(Math.random() * 3) + count;
     // ACCOUNT FOR DUPLICATES
-    // while (!imageIndicies.includes(imageNumber)) {
-    //     imageNumber = Math.floor(Math.random() * 3) + count;
-    // }
-    // // remove element from array
-    // var imageIndicies = arrayRemove(imageIndicies, imageNumber);
+    while (!imageIndicies.includes(imageNumber)) {
+        imageNumber = Math.floor(Math.random() * 3) + count;
+    }
+    // remove element from array
+    arrayRemoveAlt(imageIndicies, imageNumber);
     return imageNumber;
 }
 
@@ -63,12 +67,34 @@ function renderHTMLFile(employees) {
                         imageIndicies = [1, 2, 3, 4, 5, 6];
                     }
 
+                    var presets = {
+                        Male: {
+                            Elliot: 1,
+                            "Mr. Robot": 2,
+                            Tyrell: 3,
+                        },
+                        Female: {
+                            Darlene: 4,
+                            Dominique: 5,
+                            Angela: 6,
+                        },
+                    };
+
+                    var name = employee.getName();
                     // LOGIC FOR GENDER
                     // MALE = 1, 2, 3; FEMALE = 4, 5, 6
                     if (gender == "Male") {
                         imageNumber = getIndicies(1);
+                        // see if it matches the actual characters
+                        if (name in presets.Male) {
+                            imageNumber = presets.Male[name];
+                        }
                     } else if (gender == "Female") {
                         imageNumber = getIndicies(4);
+                        // see if it matches the actual characters
+                        if (name in presets.Female) {
+                            imageNumber = presets.Female[name];
+                        }
                     }
 
                     extra = {
@@ -76,7 +102,7 @@ function renderHTMLFile(employees) {
                         Manager: [
                             `Office: ${employee.officeNumber}`,
                             "Hello, I am a Manager within this team. The Manager is responsible for all actions taken by the organization.",
-                            "🔥 Manager",
+                            "💼 Manager",
                             // svg icon for 'office number'
                             `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
                             <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1ZM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Z"/>
@@ -86,7 +112,7 @@ function renderHTMLFile(employees) {
                         Engineer: [
                             `Github: <a href="https://github.com/${employee.github}" target="_blank">${employee.github}</a>`,
                             "Hello, I am an Engineer within this team. The Engineer is responsible for maintaining all operations within the organization.",
-                            "🔥 Engineer",
+                            "⚙️ Engineer",
                             // svg icon for 'github'
                             `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
@@ -95,7 +121,7 @@ function renderHTMLFile(employees) {
                         Intern: [
                             `School: ${employee.school}`,
                             "Hello, I am an Intern within this team. The Intern is responsible for getting coffee for the team.",
-                            "🔥 Intern",
+                            "👨🏽‍💼 Intern",
                             // svg icon for 'school'
                             `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
                             <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
